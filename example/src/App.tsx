49b87684da0935f04bb4c0e92a0343cdb9f5b8e5
Jonathan Dorety <jdorety@flur.ee>
2020-12-01 15:50:45 -0500

    Update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
-// import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-// import flureeTheme from 'fluree-mui-theme'
 
 import { FlureeQL } from 'fluree-admin-components'
-import 'fluree-admin-components/dist/index.css'
 
 const App = () => {
-  // const theme = createMuiTheme(flureeTheme)
-
   return (
-    // <ThemeProvider theme={theme}>
     <FlureeQL
       allowTransact
       _db={{
         db: 'example/mdm',
         ip: 'http://localhost:8090',
-        openApi: true,
-        environment: 'downloaded',
-        account: 'F',
         dbs: ['example/mdm']
       }}
     />
-    // </ThemeProvider>
   )
 }
 
