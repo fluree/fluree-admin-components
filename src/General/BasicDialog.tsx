c350c36f4b03b449eacb80c0ff2ad8f89fa062aa
Jonathan Dorety <jdorety@flur.ee>
2020-12-02 11:58:28 -0500

    Create basic error feedback dialog

---
+++ src/General/BasicDialog.tsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+import React, { FunctionComponent } from 'react'
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button
+} from '@material-ui/core'
+
+interface DialogProps {
+  open: boolean
+  onClose: () => void
+  message: string
+}
+
+const BasicDialog: FunctionComponent<DialogProps> = ({
+  open,
+  onClose,
+  message
+}) => {
+  return (
+    <Dialog open={open} onClose={onClose} aria-describedby='dialog-message'>
+      <DialogContent>
+        <DialogContentText id='dialog-message'>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color='primary' onClick={onClose}>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default BasicDialog
