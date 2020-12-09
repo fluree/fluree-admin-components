aaacbdd209e11b5d26ec22213562f97a6b9ca259
Jonathan Dorety <jdorety@flur.ee>
2020-12-09 17:18:45 -0500

    Break out interface declaration

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,15 +3,37 @@
  * will be overridden with file-specific definitions by rollup
  */
 declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
+  const content: { [className: string]: string }
+  export default content
 }
 
-interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+interface SvgrComponent
+  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
 declare module '*.svg' {
-  const svgUrl: string;
-  const svgComponent: SvgrComponent;
-  export default svgUrl;
+  const svgUrl: string
+  const svgComponent: SvgrComponent
+  export default svgUrl
   export { svgComponent as ReactComponent }
+}
+
+interface DBOject {
+  _id: number
+  'db/active': boolean
+  'db/id': string
+}
+
+interface DB {
+  account?: string
+  db: string | DBOject
+  dbs?: Array<string>
+  defaultPrivateKey?: any
+  displayError?: string
+  environment?: string
+  ip: string // url for fluree instance
+  loading?: boolean
+  logout?: boolean
+  openApi?: boolean
+  openApiServer?: boolean
+  token?: string
 }
