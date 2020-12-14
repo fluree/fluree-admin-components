e77d35248a8982110f654d921b18f867b551b173
Jonathan Dorety <jdorety@flur.ee>
2020-12-14 16:40:04 -0500

    Graph displays!

--- src/FlakeVis/index.tsx
+++ src/FlakeVis/index.tsx
@@ -4,8 +4,8 @@ import React, {
   useState,
   useEffect
 } from 'react'
-import { GraphComponent } from './GraphComponent'
 import { flureeFetch } from '../utils/flureeFetch'
+import { Graph } from 'react-d3-graph'
 
 interface GraphProps {
   flakes: Array<Array<any>>
@@ -13,66 +13,15 @@ interface GraphProps {
   token?: string
 }
 
-// const dummyData = {
-//   nodes: [
-//     { id: 'flake0' },
-//     { id: 'flake1' },
-//     { id: 'flake2' },
-//     { id: 'flake3' },
-//     { id: 'flake4' },
-//     { id: 'flake5' },
-//     { id: 'flake6' },
-//     { id: 'flake7' },
-//     { id: '50' },
-//     { id: '99' },
-//     { id: '100' },
-//     { id: '101' },
-//     { id: '103' },
-//     { id: '106' },
-//     { id: '107' },
-//     { id: '108' },
-//     { id: '105553116266496' },
-//     { id: '87960930223081' },
-//     { id: '-13' }
-//   ],
-//   links: [
-//     { source: 'flake0', target: '87960930223081' },
-//     { source: 'flake0', target: '50' },
-//     { source: 'flake0', target: '-13' },
-//     { source: 'flake1', target: '-13' },
-//     { source: 'flake1', target: '99' },
-//     { source: 'flake1', target: '-13' },
-//     { source: 'flake2', target: '-13' },
-//     { source: 'flake2', target: '100' },
-//     { source: 'flake2', target: '-13' },
-//     { source: 'flake3', target: '-13' },
-//     { source: 'flake3', target: '101' },
-//     { source: 'flake3', target: '105553116266496' },
-//     { source: 'flake3', target: '-13' },
-//     { source: 'flake4', target: '-13' },
-//     { source: 'flake4', target: '103' },
-//     { source: 'flake4', target: '-13' },
-//     { source: 'flake5', target: '-13' },
-//     { source: 'flake5', target: '106' },
-//     { source: 'flake5', target: '-13' },
-//     { source: 'flake6', target: '-13' },
-//     { source: 'flake6', target: '107' },
-//     { source: 'flake6', target: '-13' },
-//     { source: 'flake7', target: '-13' },
-//     { source: 'flake7', target: '108' },
-//     { source: 'flake7', target: '-13' }
-//   ]
-// }
-
 export const GraphView: FunctionComponent<GraphProps> = ({
   flakes,
   _db,
   token
 }) => {
   // const [data, setData] = useState({})
-  const [meta, setMeta] = useState<any>([])
-  const [metaObj, setMetaObj] = useState({})
-  const [flakeNodes, setFlakeNodes] = useState<Array<any>>([])
+  const [meta, setMeta] = useState<any | null>(null)
+  const [metaObj, setMetaObj] = useState<any | null>(null)
+  const [flakeNodes, setFlakeNodes] = useState<Array<any> | null>(null)
   const [graphData, setGraphData] = useState<any>(null)
   // const [mounted, setMounted] = useState(false)
 
@@ -138,10 +87,12 @@ export const GraphView: FunctionComponent<GraphProps> = ({
       const relationships: any = []
       for (const flake of flakeNodes) {
         const flakeArray = Object.values(flake)
-        console.log({ [flake.id]: flakeArray })
         flakeArray.forEach((i: any) => {
           if (metaObj[i]) {
-            relationships.push({ source: flake.id, target: metaObj[i]._id })
+            relationships.push({
+              source: flake.id,
+              target: metaObj[i]._id.toString()
+            })
           }
         })
       }
@@ -162,6 +113,8 @@ export const GraphView: FunctionComponent<GraphProps> = ({
     }
   }, [flakeNodes])
 
+  console.log({ graphData })
+
   // const relationships = []
   // for (const flake of flakes) {
   //   for (const f of Object.values(flake)) {
@@ -174,5 +127,11 @@ export const GraphView: FunctionComponent<GraphProps> = ({
   //   nodes: [...flakes, ...metadata]
   // links: [{ source: 'harry', target: 'sally' }]
   // }
-  return <div>{graphData && <GraphComponent data={graphData} />}</div>
+  return (
+    <div>
+      {meta && flakeNodes && graphData && (
+        <Graph id='flake-viz' data={graphData} config={{}} />
+      )}
+    </div>
+  )
 }
