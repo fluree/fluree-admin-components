adc8590a0008e9577c6052edefd269e24a0c2f6f
Jonathan Dorety <jdorety@flur.ee>
2021-01-14 17:09:06 -0500

    Remove old flureeFetch function from export

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Editor } from './Components/Editor'
 import { FlureeQL } from './Views/FlureeQL'
 import { History } from './Components/History'
-import { flureeFetch, gateway } from './utils/flureeFetch'
+import { gateway } from './utils/flureeFetch'
 
-export { Editor, History, flureeFetch, gateway, FlureeQL }
+export { Editor, History, gateway, FlureeQL }
